d310b05910281dbec8639fd0e00458f431b0e004
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2019-07-15 23:25:32 +0900

    add html2js

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*.js'
+      'test/**/*.js',
+      'fixtures/**/*.html'
     ],
 
 
@@ -27,6 +28,12 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'fixtures/**/*.html': ['html2js']
+    },
+
+    // Settings for html2js
+    html2JsPreprocessor: {
+      stripPrefix: 'fixtures/'
     },
 
 
--- test/canary_test.js
+++ test/canary_test.js
@@ -2,4 +2,11 @@ describe('canary', function() {
   it('canary test', function() {
     expect(1).to.be.a('number');
   });
+
+  it('canary test for html2js', function() {
+    document.body.innerHTML = __html__['headings.html'];
+    const h1 = document.querySelector('h1');
+
+    expect(h1.textContent).to.equal('Sample html for headings');
+  });
 });
