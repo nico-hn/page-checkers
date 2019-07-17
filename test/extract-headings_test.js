4329e1a52c911e1f73906e38f1277a3fa1e87ec3
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2019-07-17 20:55:10 +0900

    add a test case for a heading that includes an img element

--- test/extract-headings_test.js
+++ test/extract-headings_test.js
@@ -31,4 +31,18 @@ __[H2]Level2-2`.split(/\n/).join('\r\n');
       expect(imgs).to.equal('');
     });
   });
+
+  describe('pushHeading with images', function() {
+    const expectedHeadings = `[H1][IMG]Sample html for headings
+__[H2]Level2-1
+____[H3]Level3-1
+____[H3]Level3-2
+__[H2]Level2-2`.split(/\n/).join('\r\n');
+    it('expects to return a list of headings', function() {
+      document.body.innerHTML = __html__['headings_with_image.html'];
+      const headings = formatNodeContents(traverseNodes(document.body, pushHeading), '\r\n');
+
+      expect(headings).to.equal(expectedHeadings);
+    });
+  });
 });
