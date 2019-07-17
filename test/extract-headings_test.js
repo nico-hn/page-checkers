628b8b836d0a764b853b2953fb65f1db54b0d9d3
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2019-07-17 22:12:41 +0900

    With pushHeading(), traverseNodes() collects unformatted node data now.

--- test/extract-headings_test.js
+++ test/extract-headings_test.js
@@ -38,6 +38,22 @@ __[H2]Level2-1
 ____[H3]Level3-1
 ____[H3]Level3-2
 __[H2]Level2-2`.split(/\n/).join('\r\n');
+
+    const expectedData = {
+      tagName: 'H1',
+      text: [
+        { tagName: 'IMG', text: 'Sample html for headings' }
+      ]
+    };
+
+    it('expects to return an array of objects whose text property also is an array', function() {
+      document.body.innerHTML = __html__['headings_with_image.html'];
+
+      const headings = traverseNodes(document.body, pushHeading);
+      console.log(headings[0]);
+      expect(headings[0]).to.deep.equal(expectedData);
+    });
+
     it('expects to return a list of headings', function() {
       document.body.innerHTML = __html__['headings_with_image.html'];
       const headings = formatNodeContents(traverseNodes(document.body, pushHeading), '\r\n');
